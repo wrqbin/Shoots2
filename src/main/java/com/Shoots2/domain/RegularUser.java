359622f933ea1b28723ae0453e431f4bec5b715e
임현빈 <leobin97@gmail.com>
2025-05-13 21:02:14 +0900

    4

---
+++ src/main/java/com/Shoots2/domain/RegularUser.java
@@ -0,0 +1,172 @@
+package com.Shoots2.domain;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RegularUser implements UserDetails {
+
+    private int idx;
+    private String user_id;
+    private String password;
+    private String name;
+    private String jumin;
+    private int gender;
+    private String tel;
+    private String email;
+    private String nickname;
+    private String user_file;
+    private String register_date;
+    private String role = "common";
+    private int matchId;
+    private int userCount;
+
+    public int getUserCount() {
+        return userCount;
+    }
+
+    public void setUserCount(int userCount) {
+        this.userCount = userCount;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> list = new ArrayList<GrantedAuthority>();
+        list.add(new SimpleGrantedAuthority(this.getRole()));
+        return list;
+    }
+
+    @Override
+    public String getUsername() {
+        return user_id;
+    }
+
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJumin() {
+        return jumin;
+    }
+
+    public void setJumin(String jumin) {
+        this.jumin = jumin;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getUser_file() {
+        return user_file;
+    }
+
+    public void setUser_file(String user_file) {
+        this.user_file = user_file;
+    }
+
+    public String getRegister_date() {
+        return register_date;
+    }
+
+    public void setRegister_date(String register_date) {
+        this.register_date = register_date;
+    }
+
+    public String getRole() {
+      return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+}
